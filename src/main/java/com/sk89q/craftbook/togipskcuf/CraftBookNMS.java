4f82a56723eb2dbd6b90f7f26b86dce98a277a31
Matthew Miller <mnmiller1@me.com>
2017-11-06 13:56:05 +1000

    Initial Commit

---
+++ src/main/java/com/sk89q/craftbook/togipskcuf/CraftBookNMS.java
@@ -0,0 +1,24 @@
+package com.sk89q.craftbook.togipskcuf;
+
+import com.sk89q.craftbook.bukkit.CraftBookPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CraftBookNMS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        try {
+            CraftBookPlugin.inst().setNmsAdapter(new RealNMSAdapter());
+            System.out.println("Successfully injected CraftBookNMS!");
+        } catch (Throwable t) {
+            System.out.println("Failed to inject CraftBookNMS. Are you using the version corresponding to your MC version?");
+            t.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/com/sk89q/craftbook/togipskcuf/RealNMSAdapter.java
@@ -0,0 +1,10 @@
+package com.sk89q.craftbook.togipskcuf;
+
+import com.sk89q.craftbook.util.compat.nms.NMSAdapter;
+
+public class RealNMSAdapter extends NMSAdapter {
+
+    public boolean hasNMSExtension() {
+        return true;
+    }
+}
